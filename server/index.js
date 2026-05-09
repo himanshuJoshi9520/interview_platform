f6efbc0ece884c7603b26d51857bbd444275a022
himanshuJoshi9520 <hjoshi952080@gmail.com>
2026-05-09 20:28:10 +0530

    Update CORS configuration for deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import ttsRouter from "./routes/tts.route.js"
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }))
 
